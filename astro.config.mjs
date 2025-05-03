fd024b0da8091ef219235f2674012dad861aafab
Wassim Soltani <wasoltani@gmail.com>
2025-05-03 22:44:19 +0100

    Update project setup for Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,13 @@ import sitemap from "@astrojs/sitemap";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), sitemap()],
-  site: "https://wsoltani.github.io/",
+  site: "https://wsoltani.github.io",
+  build: {
+    assets: "assets"
+  },
+  vite: {
+    build: {
+      assetsDir: "assets"
+    }
+  }
 });
