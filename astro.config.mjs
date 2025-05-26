6f18ed62ded0d6e762b48d7937d957f8754e8be2
Wassim Soltani <wasoltani@gmail.com>
2025-05-26 21:37:56 +0100

    fix: improve mobile layout and fix canonical URL

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,13 +6,13 @@ import sitemap from "@astrojs/sitemap";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), sitemap()],
-  site: "https://wsoltani.github.io",
+  site: "https://wsoltani.com",
   build: {
-    assets: "assets"
+    assets: "assets",
   },
   vite: {
     build: {
-      assetsDir: "assets"
-    }
-  }
+      assetsDir: "assets",
+    },
+  },
 });
